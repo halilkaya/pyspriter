8c899dad597d71ae664522836cd7ecf7792b30a4
Halil Kaya <halil.kaya@metglobal.com>
2018-05-30 10:24:26 +0300

    Tabs :( are spaces now

--- pyspriter.py
+++ pyspriter.py
@@ -15,80 +15,80 @@ from os.path import isfile, join
 
 class Sprite:
 
-	folder = None
-	extension = None
-	background = None
-	direction = None
+    folder = None
+    extension = None
+    background = None
+    direction = None
 
-	def __init__(self, folder="images/", extension="jpg", background="white", direction="right"):
-		"""
-		Sprite output initialization
-		"""
-		self.folder = folder
-		self.extension = extension
-		self.background = background
-		self.direction = direction
-		self.images = [ f for f in listdir(folder) if isfile(join(folder,f)) ]
-		self.count = len(self.images)
-		self._create()
+    def __init__(self, folder="images/", extension="jpg", background="white", direction="right"):
+        """
+        Sprite output initialization
+        """
+        self.folder = folder
+        self.extension = extension
+        self.background = background
+        self.direction = direction
+        self.images = [ f for f in listdir(folder) if isfile(join(folder,f)) ]
+        self.count = len(self.images)
+        self._create()
 
-	def _loop_values(self):
-		"""
-		Gets beginning and finish values of loop for sprite direction
-		"""
-		d = self.direction
-		c = self.count
-		if d == 'right' or d == 'down':
-			return 1, c+1, 1
-		if d == 'left' or d == 'up':
-			return c, 0, -1
+    def _loop_values(self):
+        """
+        Gets beginning and finish values of loop for sprite direction
+        """
+        d = self.direction
+        c = self.count
+        if d == 'right' or d == 'down':
+            return 1, c+1, 1
+        if d == 'left' or d == 'up':
+            return c, 0, -1
 
-	def _size(self):
-		"""
-		Gets width and height of sprite
-		"""
-		width = 0
-		height = 0
-		d = self.direction
-		b,f,i = self._loop_values()
-		for i in range(b, f, i):
-			img = Image.open(self.folder + str(i) + '.' + self.extension)
-			x, y = img.size
-			if d == 'right' or d == 'left':
-				width += x
-				if y > height:
-					height = y
-			elif d == 'up' or d == 'down':
-				height += y
-				if x > width:
-					width = x
-		return width, height
+    def _size(self):
+        """
+        Gets width and height of sprite
+        """
+        width = 0
+        height = 0
+        d = self.direction
+        b,f,i = self._loop_values()
+        for i in range(b, f, i):
+            img = Image.open(self.folder + str(i) + '.' + self.extension)
+            x, y = img.size
+            if d == 'right' or d == 'left':
+                width += x
+                if y > height:
+                    height = y
+            elif d == 'up' or d == 'down':
+                height += y
+                if x > width:
+                    width = x
+        return width, height
 
-	def generate(self):
-		"""
-		Generates the sprite with given images
-		"""
-		try:
-			left = 0
-			d = self.direction
-			b,f,i = self._loop_values()
-			for i in range(b, f, i):
-				img = Image.open(self.folder + str(i) + '.' + self.extension)
-				x, y = img.size
-				if d == 'right' or d == 'left':
-					self.sprite.paste(img, (left, 0))
-					left += x
-				elif d == 'up' or d == 'down':
-					self.sprite.paste(img, (0, left))
-					left += y
-		except Exception as e:
-			return None
-		return self.sprite
+    def generate(self):
+        """
+        Generates the sprite with given images
+        """
+        try:
+            left = 0
+            d = self.direction
+            b,f,i = self._loop_values()
+            for i in range(b, f, i):
+                img = Image.open(self.folder + str(i) + '.' + self.extension)
+                x, y = img.size
+                if d == 'right' or d == 'left':
+                    self.sprite.paste(img, (left, 0))
+                    left += x
+                elif d == 'up' or d == 'down':
+                    self.sprite.paste(img, (0, left))
+                    left += y
+        except Exception as e:
+            return None
+        return self.sprite
 
-	def check_output(self, output):
-		if output:
-			return True
-		return False
+    def check_output(self, output):
+        if output:
+            return True
+        return False
 
-	def _create(self):
-		self.sprite = Image.new("RGBA", self._size())
+    def _create(self):
+        self.sprite = Image.new("RGBA", self._size())
